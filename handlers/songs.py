856b87f62c5c835eef83fee9d5c61fd8e83ec57f
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-03-30 19:55:54 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -19,7 +19,7 @@ async def a(client, message: Message):
         query += ' ' + str(i)
     print(query)
     m = await message.reply(f"**{Bn} :-** 🔍 Searching for {query}")
-    ydl_opts = {"format": "worstaudio/worst"}
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = []
         count = 0
